e21c6f294b525fb7efe8901611d6c37a8814e107
Tyuiu.PoleschukKR@outlook.com <kirillpolesuk05@gmail.com>
2023-10-18 09:54:24 +0500

    Task3

---
+++ Tyuiu.PoleschukKR.Sprint3.Task3.V19.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PoleschukKR.Sprint3.Task3.V19.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PoleschukKR.Sprint3.Task3.V19.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5fdddc20-95ce-4b33-ae64-fe3323c5f7e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PoleschukKR.Sprint3.Task3.V19/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PoleschukKR.Sprint3.Task3.V19.Lib;
+
+namespace Tyuiu.PoleschukKR.Sprint3.Task3.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Полещук К. Р. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры foreach                           *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Полещук Кирилл Романович | ИИПб-23-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Используя цикл foreach заменить буквы x на цифру 2 в строке:            *");
+            Console.WriteLine("* sxxrrg x vfrx                                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string value = "sxxrrg x vfrx";
+            char replaceable = 'x';
+            char replacement = '2';
+
+            Console.WriteLine("Исходная строка: " + value);
+            Console.WriteLine("Символ который заменяют: " + replaceable);
+            Console.WriteLine("Символ на который заменяют: " + replacement);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Конечная строка: " + ds.ReplaceCharOnNum(value, replaceable,replacement));
+            Console.ReadLine();
+
+
+        }
+
+
+    }
+}
